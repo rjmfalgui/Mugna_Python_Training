3ac6ed574ebb7b80dfe2363f255ff0e56c1e9bcd
rjmfalgui2 <rjmfalgui2@gmail.com>
2022-06-15 16:31:08 +0800

    Day 3

---
+++ exer8.py
@@ -0,0 +1,16 @@
+import time
+
+def time_decorator(func):
+    def time_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        end_time = time.perf_counter()
+        final_time = end_time - start_time
+        result = final_time * 1000
+        print(f"Time executed: {result:.5f}s.")
+        return func(*args, **kwargs)
+    return time_wrapper
+
+@time_decorator
+def adding_function(x,y):
+    print(f"The sum is {x+y}.")
+adding_function(3,5)
