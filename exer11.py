91cd749ea0f2046494a610f11b0f21d0dcb935b6
rjmfalgui2 <rjmfalgui2@gmail.com>
2022-06-25 18:57:23 +0800

    Exercise 11 Day 4

--- exer11.py
+++ exer11.py
@@ -1,25 +1,23 @@
 class Library:
-
     books = []
 
     def __init__(self, name):
         self.name = name
-    pass
 
-    def addBooks(self, book):
+    def add_books(self, book):
         self.books.append(book)
 
-    def removeBooks(self, book):
+    def remove_books(self, book):
         self.books.pop(book)
 
 library_1 = Library(name="Public Library")
 library_2 = Library(name="Private Library")
 
-library_1.addBooks("Book 3")
-library_1.addBooks("Book 5")
-library_1.addBooks("Book 63")
-library_1.addBooks("Book 75")
-library_1.addBooks("Book 43")
-library_1.addBooks("Book 55") 
+library_1.add_books("Book 3")
+library_1.add_books("Book 5")
+library_1.add_books("Book 63")
+library_1.add_books("Book 75")
+library_1.add_books("Book 43")
+library_1.add_books("Book 55") 
 
 print(library_1.books)
