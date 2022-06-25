5dcdad9d5642a5d16f5e80755acc330c25710a26
rjmfalgui2 <rjmfalgui2@gmail.com>
2022-06-25 18:54:30 +0800

    Exercise 6 Day 2 and 3

--- exer6-1.py
+++ exer6-1.py
@@ -1,12 +1,12 @@
-
 def multiplicationTable(arg1, arg2=0): 
     if arg1 == 0:
         arg2 = arg1
     
     final_list = []
-    for x in range(1, arg1+1):
+    for x in range(1, arg1 + 1):
         cont = []
-        for y in range(1, arg2+1):
+        for y in range(1, arg2 + 1):
             cont.append(x * y)
         print(cont)
+
 multiplicationTable(5, 5)
