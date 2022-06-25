e49acfbe1e8918c6b0c6df254663305458aa51e5
rjmfalgui2 <rjmfalgui2@gmail.com>
2022-06-25 18:58:11 +0800

    Exercise 13 Day 4

--- exer13pep8.py
+++ exer13pep8.py
@@ -1,7 +1,6 @@
-import math, datetime
+import math
 
 from math import sqrt
-from os import *
 
 
 def take_all_list(arg1, arg2, arg3=0, *args, **kwargs):
@@ -10,20 +9,19 @@ def take_all_list(arg1, arg2, arg3=0, *args, **kwargs):
     """
 
     list_a = [1, 2, 3, 4, 5]
-
     var_b = list_a[0] * list_a[1] + list_a[2] - (3 + list_a[3])
     print(arg1, arg2)
 
-    if not arg3 is 0:
+    if arg3 != 0:
         print(arg3)
-    if ( arg3 > 5 and arg3 < 10 ):
+    elif 5 < arg3 < 10 :
         list_a.append(arg3)
-        print(f"This is to ensure that our list would be printed ==> {list_a}") 
+        print(f'This is to ensure that our list would be printed ==> {list_a}') 
+    elif list_a is not None:
+        print(f'{list_a} has an entry')
     else:
         list_a = None
         print(var_b)
-    if list_a != None:
-        print(f"{list_a} has an entry")
 
 def print_all_number(n):
     m = 0
@@ -31,18 +29,18 @@ def print_all_number(n):
     second = n + 1
     third = n + 2
     fourth = n + 3
-    print(first, second, third, fourth)
+    print(f"{first}, {second}, {third}, {fourth}")
     for n in range(n):
         print(n)    # This will print n
-        m += n       # This will increment m with n
+        m += n      # This will increment m with n
         print(m)    # This will print m
     checker = m > sum(range(n))
     if checker:
-        print("TAMA!") 
+        print('TAMA!') 
     else:
-        print("MALI!")
+        print('MALI!')
 
-class ThisIsStudentClass():
+class ThisIsStudentClass:
     FIRSTNAME = 'A'
     LASTNAME = 'A'
 
@@ -51,18 +49,23 @@ class ThisIsStudentClass():
         self.ADDRESS = address
 
     def get_name(self):
-        return f"{self.LASTNAME}, {self.FIRSTNAME}" #"%s, %s" % (this.LASTNAME, this.FIRSTNAME) print(f"{this.LASTNAME}, {this.FIRSTNAME}") 
+        return self.LASTNAME, self.FIRSTNAME
 
 def another_function():
     # This will do something. I promise.
     print(math.pi)
     sqrt_of_pi = sqrt(math.pi)
-    print('It\'s the square root of pi', sqrt_of_pi)
-    string_sample = "Why, hello there!"
+    print(f"It's the square root of pi, {sqrt_of_pi}")
+
+    string_sample = 'Why, hello there!'
     print(string_sample[1::2])
     take_all_list(1, 2, 3)
-    try: l = 2; m = 3; n = l + m
-    except:
-        print("ERROR!")
+
+    try:
+        l = 2; 
+        m = 3; 
+        n = l + m
+    except Exception as err:
+        print(err)
         return math.pi
     take_all_list(l,m,n)
