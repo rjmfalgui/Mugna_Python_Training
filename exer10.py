f7db62a1aab072c1797f9d51288bd91ece4e923a
rjmfalgui2 <rjmfalgui2@gmail.com>
2022-06-25 18:57:06 +0800

    Exercise 10 Day 4

--- exer10.py
+++ exer10.py
@@ -1,18 +1,22 @@
 import unittest
+
 from exer7 import BankAccount
 
+
 class TestBankAccount(unittest.TestCase):
-    '''Test Cases for the class BankAccount'''
+    """
+    Test Cases for the class BankAccount
+    """
 
     def setUp(self):
         self.acct = BankAccount(name="Kobe", balance="10000")
 
-    def testCaseWithdraw(self):
+    def test_case_withdraw(self):
         balance = 3000
         test_wd = self.acct.withdraw(balance)
         self.assertEquals(test_wd, 7000)
 
-    def testCaseDeposit(self):
+    def test_case_deposit(self):
         balance = 3000
         test_dep = self.acct.deposit(balance)
         self.assertEquals(test_dep, 13000)
