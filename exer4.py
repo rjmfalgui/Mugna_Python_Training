78439f8c36e9e23c23a912a88346a456c1e667ad
rjmfalgui2 <rjmfalgui2@gmail.com>
2022-06-25 18:53:01 +0800

    Exercise 4 Day 1

--- exer4.py
+++ exer4.py
@@ -1,9 +1,7 @@
-#Count the occurences of words from a given string and store the words and corresponding count as key value pairs in a dictionary
 inp = input("Enter String: ").lower()
 i = inp.replace(".", "").replace("?", "").replace("!", "").replace(",", "").replace(":", "").replace(";", "").replace("#", "").replace("@", "").replace("&", "").replace("*", "").replace("-", "")
 
 words = i.split()
-
 container = {}
 
 for word in words:
